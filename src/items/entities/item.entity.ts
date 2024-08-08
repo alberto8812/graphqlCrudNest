fe045290aedde52e030211918cc83360c48cca30
alberto8812 <cvelascosaavedra@gamil.com>
2024-08-08 06:10:29 -0500

    create entity

--- src/items/entities/item.entity.ts
+++ src/items/entities/item.entity.ts
@@ -1,7 +1,19 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity({name:'items'})
 @ObjectType()
 export class Item {
+  
   @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @PrimaryGeneratedColumn('uuid')
+  id:string;
+
+  @Column()
+  name: string
+  @Column()
+  quentity:number;
+  @Column()
+  quentityUnits:string;
+
 }
