0d2554c13b55f7d0b8dbf4c1aa89602cc3d3939e
alberto8812 <cvelascosaavedra@gamil.com>
2024-08-07 19:14:55 -0500

    create item module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,24 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { ItemsModule } from './items/items.module';
 
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: false,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      plugins: [
+        ApolloServerPluginLandingPageLocalDefault()
+        ]
+    }),
+    ItemsModule,
+
+  ],
   controllers: [],
   providers: [],
 })
