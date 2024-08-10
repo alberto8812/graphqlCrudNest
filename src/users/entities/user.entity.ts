8a7c8dcb392226486ff9c6b1f86c0bc0d6bf02d2
alberto8812 <cvelascosaavedra@gamil.com>
2024-08-10 18:25:02 -0500

    cript password

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,40 +1,40 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ObjectType, Field, Int, ID } from '@nestjs/graphql';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
-@Entity({name:'users'})
+@Entity({ name: 'users' })
 @ObjectType()
 export class User {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
- @PrimaryGeneratedColumn('uuid')
-  id:string;
+  @Field(() => ID, { description: 'Example field (placeholder)' })
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
- @Field(()=>String)
- @Column()
- fullName:string;
- 
- @Field(()=>String)
- @Column({unique:true})
- email:string;
- 
- //@Field(()=>String)
- @Column()
- password:string;
- 
- 
- @Field(()=>[String])
- @Column({
-  type:'text',
-  array:true,
-  default:['user']
- })
- roles:string[];
- 
- @Field(()=>Boolean)
- @Column(
-  {
-    type:'boolean',
-    default:true
-  }
- )
- isActive:boolean;
+  @Field(() => String)
+  @Column()
+  fullName: string;
+
+  @Field(() => String)
+  @Column({ unique: true })
+  email: string;
+
+  //@Field(()=>String)
+  @Column()
+  password: string;
+
+
+  @Field(() => [String])
+  @Column({
+    type: 'text',
+    array: true,
+    default: ['user']
+  })
+  roles: string[];
+
+  @Field(() => Boolean)
+  @Column(
+    {
+      type: 'boolean',
+      default: true
+    }
+  )
+  isActive: boolean;
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,25 +1,34 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
 import { User } from './entities/user.entity';
 import { SignUpInput } from 'src/auth/dto/inputs/singnup.input';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import * as bcrypt from 'bcrypt'
+
 
 @Injectable()
 export class UsersService {
-
+  private logger = new Logger('UserService')
   constructor(
     @InjectRepository(User)
     private readonly userRepository: Repository<User>
   ) { }
 
   async create(signUpInput: SignUpInput): Promise<User> {
-
+    console.log(
+      {
+        password: bcrypt.hashSync(signUpInput.password, 10),
+        ...signUpInput
+      }
+    )
     try {
-      const newUser = this.userRepository.create(signUpInput)
+      const newUser = this.userRepository.create({
+        password: bcrypt.hashSync(signUpInput.password, 10),
+        ...signUpInput
+      })
       return await this.userRepository.save(newUser);
     } catch (error) {
-      console.log(error)
-      throw new BadRequestException("algo salio mal")
+      this.handleeDbError(error)
     }
 
   }
@@ -34,5 +43,16 @@ export class UsersService {
 
   block(id: string): Promise<User> {
     throw new Error(`find no implment`);
+  }
+
+  private handleeDbError(error: any): never {
+
+    if (error.code == 23505) {
+      throw new BadRequestException(error.detail.replace('key', ''))
+
+    }
+    this.logger.error(error)
+    throw new InternalServerErrorException('Please check logs')
+
   }
 }
